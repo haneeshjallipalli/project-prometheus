1005932e0b9717eba61d86cb24ebeaee0ff9d800
Haneesh55 <55525003+Haneesh55@users.noreply.github.com>
2025-08-23 22:15:21 +0530

    release v1.0

--- docker/demo-service/server.py
+++ docker/demo-service/server.py
@@ -26,4 +26,4 @@ def metrics():
     return generate_latest(), 200, {'Content-Type': 'text/plain'}
 
 if __name__ == '__main__':
-    app.run(host='0.0.0.0', port=8080)
+    app.run(host='0.0.0.0', port=8081)
